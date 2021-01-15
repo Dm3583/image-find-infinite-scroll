a7387b4e301edae9b6ab81612913a9776ba98573
Dm3583 <pankovdm@gmail.com>
2021-01-15 17:39:50 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-import str from './module-1'
+import str from './module-1';
+import './css/common.css'
 
 console.log(str);
+console.log("hello")
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    devServer: {
+        // contentBase: path.join(__dirname, 'build'),
+        compress: true,
+        port: 9000,
+        open: true,
+        stats: 'errors-only'
+    },
+
 };
