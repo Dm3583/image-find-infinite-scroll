1dfafbf0356b868bd34ad886bb5522aa0f013e36
Dm3583 <pankovdm@gmail.com>
2021-01-22 17:15:30 +0200

    add correct observer function

--- src/js/searchInterface.js
+++ src/js/searchInterface.js
@@ -15,10 +15,11 @@ let observer = new IntersectionObserver((entries, observer) => {
 
     entries.forEach(entry => {
         if (entry.isIntersecting) {
+            console.log("entry ", entry.target);
+            observer.unobserve(entry.target);
             fetchResults();
+            return;
         }
-        observer.unobserve(entry.target)
-        observer.observe(document.querySelector('li:last-child'))
     })
 }, {
     threshold: 1
@@ -46,20 +47,25 @@ function clear(element) {
     };
 };
 
+
+function scrollDown(amount) {
+    if (amount > refs.gallery.childElementCount
+        && refs.gallery.childElementCount !== 0) {
+        console.log("object ", document.querySelector('li:last-child'));
+        observer.observe(document.querySelector('li:last-child'));
+    } else {
+        observer.unobserve(document.querySelector('li:last-child'));
+        return;
+    };
+}
+
 function fetchResults() {
 
     apiService.fetchForQuery().then(data => {
         renderMarkup(cardTemplate, data.hits);
-
-        console.log(refs.gallery.childElementCount);
-        if (data.totalHits > refs.gallery.childElementCount) {
-            observer.observe(document.querySelector('li:last-child'))
-        } else {
-            observer.unobserve(document.querySelector('li:last-child'))
-            return;
-        };
-
-    }).catch(error => console.log("ERROR " + error));
+        scrollDown(data.totalHits);
+    })
+        .catch(error => console.log("ERROR " + error));
 };
 
 function search(e) {
@@ -68,6 +74,7 @@ function search(e) {
     searchBtn.disable();
     const input = e.currentTarget.elements.query;
     clear(refs.gallery);
+    observer.disconnect(refs.gallery);
 
     if (isEmptyString(input.value)) {
         clear(input);
@@ -87,43 +94,6 @@ searchBtn.enable();
 refs.searchForm.addEventListener('submit', search);
 
 
-
-
-
-
-
-
-
-
-// let ul = document.querySelector('ul')
-// let n = 1
-
-// функция создания элемента списка
-
-// function createLi() {
-//     li = document.createElement('li')
-//     li.innerHTML = `${++n} item`
-//     ul.append(li)
-// }
-
-// для того, чтобы все время наблюдать за последним элементом списка
-// мы используем нечто вроде замыкания
-// прекращаем наблюдать за целевым элементом после создания очередного li
-// и начинаем наблюдать за этим новым (последним) элементом
-
-// let observer = new IntersectionObserver((entries, observer) => {
-//     entries.forEach(entry => {
-//         if (entry.isIntersecting) {
-//             createLi()
-//         }
-//         observer.unobserve(entry.target)
-//         observer.observe(document.querySelector('li:last-child'))
-//     })
-// }, {
-//     threshold: 1
-// });
-
-// observer.observe(document.querySelector('li'))
 
 
 
